3e5094244aad53754107d022e4bb783fd90309f5
dan <wellcomefackyou@outlook.jp>
2026-05-26 19:57:31 +0900

    register.blade.php login.blade.phpファイル追加

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -29,4 +29,9 @@ class ContactController extends Controller
         Contact::create($contact);
         return view('thanks');
     }
+
+    public function register()
+    {
+        return view('register');
+    }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -7,3 +7,4 @@ use App\Http\Controllers\ContactController;
 Route::match(['get', 'post'], '/', [ContactController::class, 'index']);
 Route::post('/confirm', [ContactController::class, 'confirm']);
 Route::post('/thanks', [ContactController::class, 'thanks']);
+Route::get('/register', [ContactController::class, 'register']);
